1f2be2644285b8795c22adc2acf714f04e7ab1b8
CLS11 <crystall.101600@gmail.com>
2024-11-25 20:28:13 +0530

    Amended the coffee tile and added coffee types.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,39 @@
 import 'package:coffee_app/utilities/coffee_tile.dart';
+import 'package:coffee_app/utilities/coffee_types.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //List of coffee types
+  final List coffeeType = [
+    [
+      'Cappucino',
+      true,
+    ],
+    [
+      'Latte',
+      false,
+    ],
+    [
+      'Black',
+      false,
+    ],
+  ];
+  void coffeeTypeSelected(int index) {
+    setState(() {
+      for (int i = 0; i < coffeeType.length; i++) {
+        coffeeType[i][1] = false;
+      }
+      coffeeType[index][1] = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,13 +109,44 @@ class HomePage extends StatelessWidget {
           const SizedBox(
             height: 25,
           ),
+          //Types of coffee
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: coffeeType.length,
+              itemBuilder: (context, index) {
+                return CoffeeType(
+                  coffeeType: coffeeType[index][0],
+                  isSelected: coffeeType[index][1],
+                  onTap: () {
+                    coffeeTypeSelected(index);
+                  },
+                );
+              },
+            ),
+          ),
 
           //Listview of coffee
           Expanded(
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: [
-                CoffeeTile(),
+              children: const [
+                CoffeeTile(
+                  coffeeImagePath: 'assets/c1.jpg',
+                  coffeeName: 'Latte',
+                  coffeePrice: '4.2',
+                ),
+                CoffeeTile(
+                  coffeeImagePath: 'assets/c2.jpg',
+                  coffeeName: 'Cappucino',
+                  coffeePrice: '4.1',
+                ),
+                CoffeeTile(
+                  coffeeImagePath: 'assets/c3.jpg',
+                  coffeeName: 'Milk coffee',
+                  coffeePrice: '4.68',
+                ),
               ],
             ),
           ),
