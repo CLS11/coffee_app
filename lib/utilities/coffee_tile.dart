cb0e53865f295431063cf05bbe67b52276f39c3e
CLS11 <crystall.101600@gmail.com>
2024-11-25 20:28:35 +0530

    Added properties to change the fixed features.

--- lib/utilities/coffee_tile.dart
+++ lib/utilities/coffee_tile.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
-  const CoffeeTile({super.key});
+  const CoffeeTile({
+    required this.coffeeImagePath,
+    required this.coffeeName,
+    required this.coffeePrice,
+    super.key,
+  });
+  final String coffeeImagePath;
+  final String coffeeName;
+  final String coffeePrice;
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +26,54 @@ class CoffeeTile extends StatelessWidget {
           color: Colors.black54,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset('assets/c1.jpg'),
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(coffeeImagePath),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 8,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    coffeeName,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    'With Almond Milk',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    coffeePrice,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
